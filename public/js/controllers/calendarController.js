ce5ea66d41ac81b6e100b6b4f909c5ed2ef6a53e
jvitorn <joaoeuno2011@gmail.com>
2020-06-08 09:17:49 -0300

    adicionando funcao de mes/dia/ano

--- public/js/controllers/calendarController.js
+++ public/js/controllers/calendarController.js
@@ -1,3 +1,24 @@
 angular.module('tapapp').controller('CalendarController', function ($scope, $http, $location) {
+    const d = new Date()
 
+    $scope.date = new Date()
+
+    $scope.dateMonth = {}
+    //capturando todos os meses do ano
+    for (let contador = 0; contador <= 11; contador++) {
+        $scope.dateMonth[contador] = d.setMonth(contador)
+
+    }
+    $scope.dateWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+
+
+
+    const getDaysInMonth = (month, year) => {
+        return new Date(year, month, 0).getDate();
+    };
+    console.log(getDaysInMonth(1, 2020));
+    console.log(getDaysInMonth(2, 2020));
+    console.log(getDaysInMonth(6, 2020));
+    console.log(getDaysInMonth(12, 2020));
 });
