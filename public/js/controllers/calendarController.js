6d152f4c29409f0fe9a3bbf34ca8827156d4eda0
jvitorn <joaoeuno2011@gmail.com>
2020-06-08 17:07:42 -0300

    retirando api de feriados

--- public/js/controllers/calendarController.js
+++ public/js/controllers/calendarController.js
@@ -27,31 +27,6 @@ angular.module('tapapp').controller('CalendarController', function ($scope, $htt
         $scope.dateDay[contador] = contador
 
     }
-    $scope.test = {}
-    //api de feriados 
-    $http.get('http://api.calendario.com.br/?json=true&ano=' + d.getFullYear() + '&estado=SP&cidade=ITANHAEM&token=am9hb2V1bm8yMDExQGdtYWlsLmNvbSZoYXNoPTI0NTcxNjc4Nw')
-        .then(results => {
-            const resultados = results.data.length - 1;
-            $scope.dateApi = results.data;
 
-
-            for (let contador = 0; contador <= resultados; contador++) {
-                //conversor de string para data
-                const datas = $scope.dateApi[contador]['date'];
-                const parts = $scope.dateApi[contador]['date'].split('/');
-
-                $scope.dateApi[contador].date = new Date(parts[2], parts[1] - 1, parts[0])
-
-
-
-            }
-
-            for (let contador = 0; contador <= resultados; contador++) {
-                console.log($scope.dateApi[contador].date)
-
-            }
-
-
-        })
 
 });
