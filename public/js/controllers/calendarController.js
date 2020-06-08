d5c646826c0e9533ab4e1099d0b848dc69b768ff
jvitorn <joaoeuno2011@gmail.com>
2020-06-08 13:54:54 -0300

    corrigido - problema cm a api

--- public/js/controllers/calendarController.js
+++ public/js/controllers/calendarController.js
@@ -31,20 +31,27 @@ angular.module('tapapp').controller('CalendarController', function ($scope, $htt
     //api de feriados 
     $http.get('http://api.calendario.com.br/?json=true&ano=' + d.getFullYear() + '&estado=SP&cidade=ITANHAEM&token=am9hb2V1bm8yMDExQGdtYWlsLmNvbSZoYXNoPTI0NTcxNjc4Nw')
         .then(results => {
-            const resultados = results.data.length;
+            const resultados = results.data.length - 1;
             $scope.dateApi = results.data;
+
 
             for (let contador = 0; contador <= resultados; contador++) {
                 //conversor de string para data
-                const parts = $scope.dateApi[contador].date.split('/');
+                const datas = $scope.dateApi[contador]['date'];
+                const parts = $scope.dateApi[contador]['date'].split('/');
 
                 $scope.dateApi[contador].date = new Date(parts[2], parts[1] - 1, parts[0])
+
+
+
+            }
+
+            for (let contador = 0; contador <= resultados; contador++) {
+                console.log($scope.dateApi[contador].date)
 
             }
 
 
-
         })
-        .catch(error => console.log(error))
 
 });
