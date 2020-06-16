0ab52422cb9b9554cbce439510807fa7de71c3fa
jvitorn <joaoeuno2011@gmail.com>
2020-06-16 12:10:24 -0300

    adicionando dados no sidebar

--- public/js/controllers/rootController.js
+++ public/js/controllers/rootController.js
@@ -2,6 +2,8 @@ angular.module('tapapp').run(function ($rootScope, $http, $document, $compile) {
 
     $rootScope.api = 'http://localhost:3332/';
 
+    $rootScope.name = localStorage.getItem('userName');
+    $rootScope.email = localStorage.getItem('email');
 
     $rootScope.notfluid = true;
     $rootScope.login = true;
